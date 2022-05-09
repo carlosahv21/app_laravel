c53d7e515b71483cae22ae236b102d4fa041e68a
Aldrha Petit <aldrha@gmail.com>
2022-05-08 22:53:34 -0400

    comienzo de JOIN-17, falta colocarle los datos reales

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,8 @@
     <!-- Apex Charts -->
     <link type="text/css" href="/vendor/apexcharts/apexcharts.css" rel="stylesheet">
     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- Datepicker -->
     <link id="bsdp-css" href="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker3.min.css" rel="stylesheet">
 
@@ -30,8 +31,8 @@
     @livewireStyles
 
     <!-- Core -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 
     <!-- Vendor JS -->
     <script src="/assets/js/on-screen.umd.min.js"></script>
@@ -78,6 +79,8 @@
 
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/a5347400c8.js" crossorigin="anonymous"></script>
+
+
 
 </head>
 
@@ -198,6 +201,8 @@
                 }
             });
 
+            
+             
         });
 
     </script>
